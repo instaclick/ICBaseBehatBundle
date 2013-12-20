93d67c54ec6b6baeee56f7b2b83753e889333a4a
Mark Kasaboski <markk@nationalfibre.net>
2013-12-20 11:44:13 -0500

    DE110-456 Fixed bug in CsrContext: ExpectationException requires session as second parameter to constructor. Added spin to CsrContext::assertOldestJob() and added spin to QueueStepContext::doItForEach().

--- Context/QueueStepContext.php
+++ Context/QueueStepContext.php
@@ -7,9 +7,7 @@ namespace IC\Bundle\Base\BehatBundle\Context;
 
 use Behat\Behat\Context\Step;
 use Behat\Behat\Exception\AmbiguousException;
-
 use Behat\Gherkin\Node\TableNode;
-
 use Behat\MinkExtension\Context\RawMinkContext;
 
 //
@@ -22,13 +20,14 @@ require_once 'PHPUnit/Framework/Assert/Functions.php';
  * Queue step subcontext
  *
  * @author Anthon Pang <anthonp@nationalfibre.net>
+ * @author Mark Kasaboski <markk@nationalfibre.net>
  */
 class QueueStepContext extends RawMinkContext
 {
     /**
      * @var array Queued steps
      */
-    private $steps = array();
+    public $steps = array();
 
     /**
      * I will (be) ...
@@ -100,27 +99,31 @@ class QueueStepContext extends RawMinkContext
      */
     public function doItForEach($table)
     {
-        $steps = array();
+        $that = $this;
 
-        $situations = $table->getHash();
-        foreach ($situations as $situation) {
-            foreach ($this->steps as $step) {
-                if (preg_match_all('/(\{[^}]+\})/', $step, $matches)) {
-                    foreach ($matches[0] as $key) {
-                        $nakedKey = substr($key, 1, -1);
+        $this->getMainContext()->getSubcontext('SpinCommandContext')->spin(function () use ($table, $that) {
+            $steps      = array();
+            $situations = $table->getHash();
 
-                        if (isset($situation[$nakedKey])) {
-                            $step = str_replace($key, $situation[$nakedKey], $step);
+            foreach ($situations as $situation) {
+                foreach ($that->steps as $step) {
+                    if (preg_match_all('/(\{[^}]+\})/', $step, $matches)) {
+                        foreach ($matches[0] as $key) {
+                            $nakedKey = substr($key, 1, -1);
+
+                            if (isset($situation[$nakedKey])) {
+                                $step = str_replace($key, $situation[$nakedKey], $step);
+                            }
                         }
                     }
+
+                    $steps[] = new Step\Then($step);
                 }
-
-                $steps[] = new Step\Then($step);
             }
-        }
 
-        $this->steps = array();
+            $that->steps = array();
 
-        return $steps;
+            return $steps;
+        });
     }
 }
